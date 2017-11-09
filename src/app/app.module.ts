bf5e2c6c149a553d19bde672a33667f955b7b693
Xposean <843095666@qq.com>
2017-11-09 22:34:47 +0800

    解决编译后刷新 error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,13 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import {AppRoutingModule} from "./app-routing.module";
 
-import {IconModule} from 'freeng/freeng';
+import {IconModule} from 'freeng/component/icon/icon.component';
 import { HeaderComponent } from './header/header.component';
 import {GetDataService} from "./serve/get-data.service";
 import { DayPipePipe } from './pipe/day-pipe.pipe';
 import { DatePipePipe } from './pipe/date-pipe.pipe';
 import { ListComponent } from './list/list.component';
-import {LoadingModule} from 'freeng/freeng';
+import {LoadingModule} from 'freeng/component/loading/loading.component';
 import { DetailsComponent } from './details/details.component';
 import { MusicDetailsComponent } from './music-details/music-details.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
@@ -21,6 +21,7 @@ import {SidenavModule} from "freeng/component/sidenav/sidenav.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CookieService} from "angular2-cookie/services/cookies.service";
 import { ImageTextDetailsComponent } from './image-text-details/image-text-details.component';
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,7 +45,7 @@ import { ImageTextDetailsComponent } from './image-text-details/image-text-detai
     LoadingModule,
     SidenavModule
   ],
-  providers: [GetDataService,CookieService],
+  providers: [GetDataService,CookieService, { provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
