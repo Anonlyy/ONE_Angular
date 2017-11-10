df85594e81adf43fc711c43d6476f68971548698
Xposean <843095666@qq.com>
2017-11-10 23:06:32 +0800

    更换angular-cli版本

--- src/app/pipe/day-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DayPipePipe } from './day-pipe.pipe';
-
-describe('DayPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DayPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
