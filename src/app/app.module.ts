e4b7bdeda12b446c2260eb7f1a3b8c7e4491c8e0
Xposean <843095666@qq.com>
2017-11-10 17:14:48 +0800

    修改菜单栏

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,17 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import {AppRoutingModule} from "./app-routing.module";
 
-import {IconModule} from 'freeng/component/icon/icon.component';
 import { HeaderComponent } from './header/header.component';
 import {GetDataService} from "./serve/get-data.service";
 import { DayPipePipe } from './pipe/day-pipe.pipe';
 import { DatePipePipe } from './pipe/date-pipe.pipe';
 import { ListComponent } from './list/list.component';
-import {LoadingModule} from 'freeng/component/loading/loading.component';
+import { LoadingModule } from 'freeng/freeng'
 import { DetailsComponent } from './details/details.component';
 import { MusicDetailsComponent } from './music-details/music-details.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import {SidenavModule} from "freeng/component/sidenav/sidenav.component";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {CookieService} from "angular2-cookie/services/cookies.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CookieService,CookieOptions } from 'angular2-cookie/core';
 import { ImageTextDetailsComponent } from './image-text-details/image-text-details.component';
 import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 @NgModule({
@@ -41,11 +39,9 @@ import {LocationStrategy, HashLocationStrategy} from "@angular/common";
     HttpModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    IconModule,
-    LoadingModule,
-    SidenavModule
+    LoadingModule
   ],
-  providers: [GetDataService,CookieService, { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [GetDataService,CookieService,{ provide: CookieOptions, useValue:{}}, { provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,22 +12,22 @@ export class HeaderComponent implements OnInit {
     {
       icon:'czs-newspaper-l',
       name:'图文',
-      link:'0',
+      link:"0",
     },
     {
       icon:'czs-book-l',
       name:'阅读',
-      link:'1',
+      link:"1",
     },
     {
       icon:'czs-music-note-l',
       name:'音乐',
-      link:'4',
+      link:"4",
     },
     {
       icon:'czs-network-l',
       name:'影视',
-      link:'5',
+      link:"5",
     }
   ]
   constructor(private router:Router) { }
@@ -35,9 +35,12 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
-  toList(i:number){
-    // this.router.navigate(['/list',this.menuList[i].link]);
+  toList(){
+    // this.router.navigate([this.menuList[i].link,this.menuList[i].key]);
     this.isShow = false;
+  }
+  toAbout(){
+    window.location.href = 'http://www.xposean.xin/about/';
   }
   showMenu(){
     this.isShow = true;
